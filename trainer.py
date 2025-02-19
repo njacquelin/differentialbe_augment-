c53922cd7065393a955717391107bb8d75d8e2e1
njacquelin <nicolas.jacquelin@insa-lyon.fr>
2025-02-19 15:29:32 +0000

    added some logs

--- trainer.py
+++ trainer.py
@@ -9,20 +9,22 @@ from torch.optim import SGD
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import save_image
 
+from time import time
+
 
 if __name__ == "__main__":
 
     writer = SummaryWriter()
 
-    batch_size = 3
+    batch_size = 1024
     epochs_nb = 100
-    warmup_epochs_nb = 10
-    lr = 1e-3
+    # warmup_epochs_nb = 10
+    lr = 1e-2
 
-    # hardware = "mono-gpu"
-    hardware = "cpu"
+    hardware = "mono-gpu"
+    # hardware = "cpu"
     computer = "pagoda"
-    nb_workers = 10
+    nb_workers = 20
     # scaler = torch.cuda.amp()
 
     global_rank, local_rank, world_size, is_master, device = get_infos(hardware, computer)
@@ -30,6 +32,7 @@ if __name__ == "__main__":
 
     model = Image_Augmenter(cifar=True)
     model = adapt_to_parallel_computing(hardware, model, local_rank)
+    model.to(device)
     
     train_dataset = get_dataset(device, "cifar", 'train')
     eval_dataset = get_dataset(device, "cifar", 'eval')
@@ -56,19 +59,27 @@ if __name__ == "__main__":
     #     last_epoch = last_step, eta_min=1e-8
     # )
 
-    for epoch in range(epochs_nb):
+    for epoch in range(1, epochs_nb+1):
         ### TRAIN ###
         losses = []
+        start = time()
         for i, batch in enumerate(train_loader):
             img_0, img_t, params = batch
-            img_out = model(img_0, params)
 
+            img_0 = img_0.to(device)
+            img_t = img_t.to(device)
+            params = params.to(device)
+        
+            img_out = model(img_0, params)
+            
             loss = criterion(img_out, img_t)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             losses.append(loss.float())
+
+            print(f"Epoch {epoch} - batch {i+1}/{len(train_loader)}")
             
             # ### LOCAL TESTS BLOC => TO REMOVE ###
             # img_0 = reverse_transform(img_0[0])
@@ -83,6 +94,7 @@ if __name__ == "__main__":
             
         avg_loss = sum(losses) / len(losses)
         writer.add_scalar('train_loss_z', avg_loss, epoch)
+        print(f"Epoch {epoch} - train loss: {avg_loss:2.5f} - duration: {time() - start:5.5f}s")
 
         img_0 = reverse_transform(img_0[0])
         img_t = reverse_transform(img_t[0])
@@ -90,33 +102,41 @@ if __name__ == "__main__":
         writer.add_image(f"train source epoch {epoch}", img_0)
         writer.add_image(f"train target epoch {epoch}", img_t)
         writer.add_image(f"train output epoch {epoch}", img_out)
+        
         img_save = torch.stack((img_0, img_t, img_out), dim=0)
         save_image(img_save,
-                   f'img/train images epoch {epoch+1}.jpg')
-
+                   f'img/train images epoch {epoch}.jpg')
 
         ### TEST ###
         with torch.no_grad():
             losses = []
+            start = time()
             for i, batch in enumerate(eval_loader):
                 img_0, img_t, params = batch
-                img_out = model(img_0, params)
 
+                img_0 = img_0.to(device)
+                img_t = img_t.to(device)
+                params = params.to(device)
+
+                img_out = model(img_0, params)
+                
                 loss = criterion(img_out, img_t)
                 losses.append(loss.float())
 
-            avg_loss = sum(losses) / len(losses)
+                print(f"Epoch {epoch} - batch {i+1}/{len(eval_loader)}")
+
+        avg_loss = sum(losses) / len(losses)
         writer.add_scalar('train_loss_z', avg_loss, epoch)
+        print(f"Epoch {epoch} - test loss : {avg_loss:2.5f} - duration: {time() - start:5.5f}s")
 
         img_0 = reverse_transform(img_0[0])
         img_t = reverse_transform(img_t[0])
         img_out = reverse_transform(img_out[0])
         writer.add_image(f"train source epoch {epoch}", img_0)
-        writer.add_image(f"train target epoch {epoch}", img_t[0])
-        writer.add_image(f"train output epoch {epoch}", img_out[0])
+        writer.add_image(f"train target epoch {epoch}", img_t)
+        writer.add_image(f"train output epoch {epoch}", img_out)
+        
         img_save = torch.stack((img_0, img_t, img_out), dim=0)
         save_image(img_save,
-                   f'img/test images epoch {epoch+1}.jpg')
-
-
+                   f'img/test images epoch {epoch}.jpg')
 
